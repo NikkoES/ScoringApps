ee7b7ec7da9ec11f1a94df9c2e66e8f440be1b9f
NikkoES <nikkoeka04@gmail.com>
2018-01-31 17:11:56 +0700

    tambah list

---
+++ app/src/main/java/camp/bso/inf/scoringapps/ListPertandinganActivity.java
@@ -0,0 +1,13 @@
+package camp.bso.inf.scoringapps;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ListPertandinganActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_pertandingan);
+    }
+}
